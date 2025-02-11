9976d3f60dac7cfc691e9d02dee4cf2f2c76e80f
vincehudak <ecniv.hudak@gmail.com>
2025-02-11 17:04:34 +0000

    fixed model copy script

--- model_copy.sh
+++ model_copy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Define the source directory
-SOURCE_DIR="root"
+SOURCE_DIR="/home/s2209005/Pytorch_workshop/agnews/model"
 
 # Get the parent directory of the source
 PARENT_DIR=$(dirname "$(realpath "$SOURCE_DIR")")
 
 # Loop to create 8 copies
 for i in {1..8}; do
-    DEST_DIR="${PARENT_DIR}/root_${i}"
+    DEST_DIR="${PARENT_DIR}/model_${i}"
     rsync -a "$SOURCE_DIR/" "$DEST_DIR/"
     echo "Copied $SOURCE_DIR to $DEST_DIR using rsync"
 done
